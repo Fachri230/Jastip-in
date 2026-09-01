3c02fb36b49b97c86910f26b12279869ecad8d3b
Muhammad Fachri Nadhir Amrullah <fachri726game@gmail.com>
2026-09-01 14:31:47 +0700

    Dashboard Siswa Proto

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/css/app.css')
+
 </head>
-<body>
-    <h1>A</h1>
+
+<body class="bg-gray-100 scrollbar-none scroll-smooth">
+    <div class="bg-green-700 h-10 fixed pr-255">
+        <li class="flex flex-inline leading-5 text-xl text-center text-white font-semibold italic">
+            <svg class="ml-2" width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                stroke="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path>
+                    <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path>
+                    <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path>
+                </g>
+            </svg>
+            <a class="ml-118 h-10 rounded-xs hover:bg-green-900 p-2 hover:duration-350 hover:delay-150" href="">Home</a>
+            <a class="h-10 rounded-xs hover:bg-green-900 p-2 hover:duration-350 hover:delay-150" href="">Store</a>
+            <a class="h-10 rounded-xs hover:bg-green-900 p-2 hover:duration-350 hover:delay-150" href="">About</a>
+        </li>
+    </div>
 </body>
+
 </html>
